4344310cf63474445aa2292d478e061bb3c6fbea
P. Douglas Reeder <reeder.29@gmail.com>
2022-01-04 22:25:06 -0500

    New notes start w/ title block; search words appended to new note are italicized

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
   const focusOnLoad = useRef(false);   // no re-render when changed
   async function addNote() {
     try {
-      const initialText = searchStr.trim() ? `<p></p><p>${searchStr.trim()} </p>` : "";
+      const initialText = searchStr.trim() ? `<h1></h1><p></p><p><em>${searchStr.trim()}</em></p>` : "<h1></h1><p></p>";
       const newNote = createMemoryNote(null, initialText, null, 'text/html;hint=SEMANTIC');
       // console.log("adding note:", newNote);
       await upsertNote(newNote);
