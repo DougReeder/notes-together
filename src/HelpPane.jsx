a8654e50a1892c36dde7e89b3456b364a11c1d60
P. Douglas Reeder <reeder.29@gmail.com>
2024-04-20 16:10:21 -0400

    Updates Help re: offline devices and sync

--- src/HelpPane.jsx
+++ src/HelpPane.jsx
@@ -71,6 +71,9 @@ mobile: ${navigator.userAgentData?.mobile || ''}` +
              viewBox="0 0 739 853"> <g> <polygon className="rs-logo-shape"
                                                  points="370,754 0,542 0,640 185,747 370,853 554,747 739,640 739,525 739,525 739,476 739,427 739,378 653,427 370,589 86,427 86,427 86,361 185,418 370,524 554,418 653,361 739,311 739,213 739,213 554,107 370,0 185,107 58,180 144,230 228,181 370,100 511,181 652,263 370,425 87,263 87,263 0,213 0,213 0,311 0,378 0,427 0,476 86,525 185,582 370,689 554,582 653,525 653,590 653,592 "></polygon> </g> </svg>
         <p>You can use the same account with <a href="https://remotestorage.io/apps/" target="_blank"  rel="noreferrer">multiple apps</a>.</p>
+        <p>When you edit a note on a device that is off-line, get the device back on-line (and give it ten seconds to sync)
+          before editing the <em>same</em> note on a different device. Otherwise, the changes from one device may be lost.
+        You can edit <em>different</em> notes on different devices without concern.</p>
 
         <h4>How closely must search words match the text?</h4>
         <ul>
